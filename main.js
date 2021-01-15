02a053ccaf2b652a7f69c3738a149dbb1994b83e
dnotrad <64872689+dnotrad@users.noreply.github.com>
2021-01-15 14:13:02 +0300

    new

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 let burger = document.querySelector(".burger");
 let mobileNav = document.querySelector(".nav-for-mobile__wrapper");
 
+let contacts = document.querySelector(".contacts");
+
+let scrollTo = () => {
+    // e.preventDefault();
+    // element.scrollIntoView();
+    alert(5);
+}
+
 burger.addEventListener("click", () => {
     burger.classList.toggle("active");
     mobileNav.classList.toggle("active");
+    document.body.classList.toggle("fixed");
 })
