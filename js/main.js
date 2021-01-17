8d8775cd762dad2135a94557566ca0ee78608af8
dnotrad <64872689+dnotrad@users.noreply.github.com>
2021-01-17 15:16:25 +0300

    add animation

--- js/main.js
+++ js/main.js
@@ -44,3 +44,34 @@ links.forEach( (link) => {
         removeClassesForBurger();
     })
 } )
+
+// anim-item
+
+let animItems = document.querySelectorAll(".anim-item");
+// ПОЛУЧИТЬ РАССТОЯНИЕ ЭЛЕМЕНТА ОТ НАЧАЛА СТРАНИЦЫ
+function offset(el) {
+    var rect = el.getBoundingClientRect(),
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+}
+
+if (animItems.length > 0) {
+    window.addEventListener("scroll", animOnScroll)
+    function animOnScroll(){
+        for ( i = 0; i < animItems.length; i++ ){
+            const animItem = animItems[i];
+            const animItemHeightFromTop = offset(animItem).top;
+            const animItemHeight = animItem.offsetHeight;
+            let koef = window.innerHeight - animItemHeight / 4;
+            if(animItemHeight > window.innerHeight){
+                koef = (3/4) * window.innerHeight;
+            } 
+
+            if( window.pageYOffset > animItemHeightFromTop - koef){
+                animItem.classList.add("active");
+            }else animItem.classList.remove("active");
+        }
+    }
+}
+animOnScroll();
